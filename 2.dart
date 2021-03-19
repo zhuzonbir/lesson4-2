0b993771169b2c153aa402bf01dce4f1d93585ba
zhuzonbir <phnix11.11@gmail.com>
2021-03-19 21:33:42 +0600

    done

---
+++ 2.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {
+
+ stdout.write("как тебя зовут?");
+  String nname = stdin.readLineSync()!;
+  stdout.write("сколько тебе лет?");
+  String age = stdin.readLineSync()!;
+  
+  print("как тебя зовут $nname, сколько тебе лет $age ");
+
+  // stdout.write("впишите число");
+  // print(int.parse(stdin.readLineSync()!) % 2 == 0 ? "четное" : "нечетное");
+
+//   List array = [1, 2, 3, 4, 78, 76];
+//   for (int i = 0; i < array.length; i++) {
+//     // print(array[i]);
+
+//     if (array[i] < 5) {
+//       print(array[i]);
+//     }
+//      else print(array[i]);
+//   }
+ }
